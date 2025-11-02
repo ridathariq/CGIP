bfb6cf041cc101e534860a70164ca564a4c3634d
ridathariq <153930862+ridathariq@users.noreply.github.com>
2025-11-02 20:02:47 +0530

    Fix indentation and formatting issues in animation code

--- 1balloon.py
+++ 1balloon.py
@@ -5,18 +5,17 @@ fig, ax = plt.subplots()
 ax.set_xlim(0, 200)
 ax.set_ylim(0, 200)
 
-# Balloon
 b1 = plt.Circle((50, -10), 10, color='red')
 ax.add_patch(b1)
 b2 = plt.Circle((90, -10), 10, color='blue')
 ax.add_patch(b2)
-# String
+
 string1, = ax.plot([50, 50], [0, 0], color='black')
 string2, = ax.plot([90, 90], [0, 0], color='black')
 def update(frame):
-    y_pos = -10 + frame * 2  # Starts below screen, rises up
+    y_pos = -10 + frame * 2  
     
-    # Update balloon and string
+
     b1.center = (50, y_pos)
     string1.set_data([50, 50], [0, y_pos - 10])
     b2.center = (90, y_pos)
@@ -24,4 +23,5 @@ def update(frame):
     return b1, string1,b2,string2
 
 ani = FuncAnimation(fig, update, frames=100, interval=30, blit=True)
+
 plt.show()
