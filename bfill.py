49fe7c1742c029d03237b79588d72a86e06d78b3
ridathariq <153930862+ridathariq@users.noreply.github.com>
2025-11-02 19:59:12 +0530

    Update bfill.py

--- bfill.py
+++ bfill.py
@@ -10,12 +10,12 @@ def boundary_fill(img, x, y, fill, boundary):
             boundary_fill(img, x, y+1, fill, boundary)
             boundary_fill(img, x, y-1, fill, boundary)
 
-# Create image and rectangle boundary
+
 img = Image.new("RGB", (100, 100), "white")
 draw = ImageDraw.Draw(img)
 draw.rectangle([30, 30, 70, 70], outline="black")
 
-# Apply boundary fill
 boundary_fill(img, 50, 50, (255, 0, 0), (0, 0, 0))
 
 img.show()
+
