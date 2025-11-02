7c332d8c7fa858eed1b33eac0668286abed20cba
ridathariq <153930862+ridathariq@users.noreply.github.com>
2025-11-02 20:03:14 +0530

    Update 1car_hill_dda.py

--- 1car_hill_dda.py
+++ 1car_hill_dda.py
@@ -23,16 +23,15 @@ ax.set_xlim(0, 200)
 ax.set_ylim(0, 100)
 ax.set_aspect('equal')
 
-# Draw road using DDA
+
 road_x, road_y = dda_line(0, 30, 200, 30)
 ax.plot(road_x, road_y, color='gray', linewidth=20)
 
-# Draw road markings using DDA
+
 for i in range(0, 200, 30):
     mark_x, mark_y = dda_line(i, 30, i+15, 30)
     ax.plot(mark_x, mark_y, color='white', linewidth=3)
 
-# Draw hills using DDA
 hill1_x, hill1_y = dda_line(0, 50, 80, 80)
 hill2_x, hill2_y = dda_line(80, 80, 160, 50)
 hill3_x, hill3_y = dda_line(120, 50, 200, 70)
@@ -41,7 +40,7 @@ ax.plot(hill1_x, hill1_y, color='green', linewidth=4)
 ax.plot(hill2_x, hill2_y, color='darkgreen', linewidth=4)
 ax.plot(hill3_x, hill3_y, color='green', linewidth=4)
 
-# Car
+
 car_body = plt.Rectangle((10, 35), width=30, height=10, facecolor='red')
 wheel1 = plt.Circle((15, 30), radius=4, facecolor='black')
 wheel2 = plt.Circle((35, 30), radius=4, facecolor='black')
@@ -51,7 +50,7 @@ ax.add_patch(wheel1)
 ax.add_patch(wheel2)
 
 def update(frame):
-    # Move car
+
     x_offset = frame * 2
     car_body.set_xy((10 + x_offset, 35))
     wheel1.set_center((15 + x_offset, 30))
@@ -60,4 +59,5 @@ def update(frame):
     return car_body, wheel1, wheel2
 
 ani = FuncAnimation(fig, update, frames=range(0, 80), interval=50, blit=True, repeat=True)
+
 plt.show()
